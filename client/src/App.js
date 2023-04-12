10763c8606dd09ddf1da9f8b29492c9a5a342249
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-11 20:30:26 -0500

    Database Entry
    Added code to where our inputs now save to the database itself.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,46 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // State variables to store the food name and days since I ate
+  const [foodName, setfoodName] = useState("");
+  const [days, setDays] = useState(0);
+
+  // Function to add the food name and days since I ate to the list when the button is clicked
+  const addToList = () => {
+    // console.log(foodName + days)
+    Axios.post('http://localhost:3001/insert', {
+      foodName: foodName, 
+      days: days
+    })
+  }
+
   return (
     <div className="App">
       <h1>Crud APP with MERN</h1>
-
+    
+      {/* ------------------------------------- */}
+      {/* Main Section */}
       <label>Food Name:</label>
-      <input type="text"></input>
+
+      <input
+       type="text" onChange={(event) => {
+        setfoodName(event.target.value)
+        }}>       
+      </input>
+
       <label>Days Since I Ate:</label>
-      <input type="number"></input>
-      <button>Add to List</button>
+      <input
+       type="number" onChange={(event) => {
+        setDays(event.target.value)
+        }}>
+      </input>
+
+      <button onClick={addToList}>Add to List</button>
+      {/* ------------------------------------- */}
+
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 // Import the food model from the food.js file in the models folder
@@ -7,6 +8,7 @@ const FoodModel = require('./models/food');
 
 // Middleware to parse the body of the request as JSON
 app.use(express.json())
+app.use(cors())
 
 // Connect to the database
 mongoose.connect('mongodb+srv://newuser:pass1234@crud.0pme8m8.mongodb.net/food', 
@@ -15,9 +17,12 @@ mongoose.connect('mongodb+srv://newuser:pass1234@crud.0pme8m8.mongodb.net/food',
 });
 
 // Create a new food document in the database with the foodName and daysSinceIAte fields
-app.get('/', async (req, res) => {
+app.post('/insert', async (req, res) => {
     // Find all the food in the database
-    const food = new FoodModel({foodName: "Apple", daysSinceIAte: 3});
+    const foodName = req.body.foodName;
+    const days = req.body.days;
+
+    const food = new FoodModel({foodName: foodName, daysSinceIAte: days});
 
     try {
         // Save the food to the database
