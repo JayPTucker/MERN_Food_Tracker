7acf6b44904890dd68eab63c5b12b86b8be1ad38
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-11 19:40:59 -0500

    Created Food.js server file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 
+app.use(express.json())
 
-
+mongoose.connect('mongodb+srv://newuser:test1234@crud.0pme8m8.mongodb.net/?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true,
+})
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
---
+++ server/models/Food.js
@@ -0,0 +1,16 @@
+const mongoose = reuqire('mongoose');
+
+const foodSchema = new mongoose.Schema({
+    foodName: {
+        type: String,
+        required: true,
+    },
+    daysSinceIAte: {
+        type: Number,
+        required: true,
+    },
+});
+
+// Passes in the name of the model and the schema to use for the model as parameters to mongoose.model()
+const Food = mongoose.model('Food', foodSchema);
+module.exports = Food;
