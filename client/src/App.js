5ce5151352327952c4380fbf6e685e67ec92ae3f
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-12 23:54:00 -0500

    Update App.js
    Updated server files to send info to localhost:3001

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
   // Function to add the food name and days since I ate to the list when the button is clicked
   const addToList = () => {
     // console.log(foodName + days)
+    // Send a POST request to the server with the food name and days since I ate
     Axios.post('http://localhost:3001/insert', {
       foodName: foodName, 
       days: days
